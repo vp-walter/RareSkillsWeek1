4a07cc960b4e1b2a724dd95d1f26cd5e05df882a
Walter Cavinaw <w.cavinaw@gmail.com>
2023-09-17 14:45:09 -0700

    fixing tests and structure of SanctionableToken

--- src/SanctionableToken.sol
+++ src/SanctionableToken.sol
@@ -1,8 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable2Step} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 
-contract SanctionableToken is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+// @notice:
+contract SanctionableToken is ERC20, Ownable2Step {
+    mapping(address => bool) _sanctionedAddresses;
+
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable2Step() {}
+
+    function addAddressToSanctionsList(address addressToSanction_)
+        external
+        payable
+        onlyOwner
+        validAddress(addressToSanction_)
+    {
+        _sanctionedAddresses[addressToSanction_] = true;
+    }
+
+    function removeAddressFromSanctionsList(address addressToRemove_)
+        external
+        payable
+        onlyOwner
+        validAddress(addressToRemove_)
+    {
+        _sanctionedAddresses[addressToRemove_] = false;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+        bool isFromSanctioned = _sanctionedAddresses[from];
+        bool isToSanctioned = _sanctionedAddresses[to];
+        require(isFromSanctioned || isToSanctioned, "address used in transfer is sanctioned");
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
--- test/SanctionableToken.t.sol
+++ test/SanctionableToken.t.sol
@@ -5,9 +5,35 @@ import {Test, console2} from "forge-std/Test.sol";
 import {SanctionableToken} from "../src/SanctionableToken.sol";
 
 contract SanctionableTokenTest is Test {
-    SanctionableToken token;
+    SanctionableToken public token;
+    address internal _bob;
+    address internal _alice;
 
     function setUp() public {
+        _alice = address(1);
+        vm.label(_alice, "Alice");
+        _bob = address(2);
+        vm.label(_bob, "Bob");
         token = new SanctionableToken("USD", "USD");
     }
+
+    function testTokenName() public {
+        assertEq(token.name(), "USD");
+    }
+
+    function testCanTransferFunds() public {}
+
+    function testAdminCanSanctionAddress() public {}
+
+    function testNonAdminCannotSanctionAddress() public {}
+
+    function testSanctionedAddressCantTransferFunds() public {}
+
+    function testSanctionedAddressCantReceiveFunds() public {}
+
+    function testCantSanctionZeroAddress() public {}
+
+    function testSanctionedAddressCantTransferFrom() public {}
+
+    function testRemoveSanctionedAddressCanTransfer() public {}
 }
