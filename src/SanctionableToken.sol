75a4f17a2127313de738cdd485f8e4939c43cc9e
Walter Cavinaw <w.cavinaw@gmail.com>
2023-09-17 15:19:41 -0700

    complete sanctionableToken.

--- src/SanctionableToken.sol
+++ src/SanctionableToken.sol
@@ -4,10 +4,14 @@ pragma solidity ^0.8.13;
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Ownable2Step} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 
-// @notice:
+/// @title Token that can prevent addresses from sending/receiving via sanctions.
+/// @author Walter Cavinaw
+/// @notice The owner of the Token can add and remove addresses that won't be able to send/receive funds.
+/// @dev Overrides the _beforeTokenTransfer to check for sanctioned addresses. zero address can't be sanctioned.
 contract SanctionableToken is ERC20, Ownable2Step {
     mapping(address => bool) _sanctionedAddresses;
 
+    /// @dev Zero address cannot be sanctioned, otherwise mint and burn operations break.
     modifier validAddress(address _addr) {
         require(_addr != address(0), "Not valid address");
         _;
@@ -15,28 +19,43 @@ contract SanctionableToken is ERC20, Ownable2Step {
 
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable2Step() {}
 
+    /// @notice Mint SanctionableTokens
+    /// @dev reverts if the address to mint to is sanctioned.
+    /// @param to_ The address of the token receiver.
+    /// @param amount_ The amount of ERC20 tokens to mint.
+    function mint(address to_, uint256 amount_) public onlyOwner {
+        _mint(to_, amount_);
+    }
+
+    /// @notice Allows owner to add address to sanctions list
+    /// @dev only the owner of the token can add an address.
+    /// @dev reverts if the address is the zero address.
+    /// @param addressToSanction_ The address to add to sanction list.
     function addAddressToSanctionsList(address addressToSanction_)
         external
-        payable
         onlyOwner
         validAddress(addressToSanction_)
     {
         _sanctionedAddresses[addressToSanction_] = true;
     }
 
-    function removeAddressFromSanctionsList(address addressToRemove_)
-        external
-        payable
-        onlyOwner
-        validAddress(addressToRemove_)
-    {
+    /// @notice Allows owner to remove an address from sanctions list
+    /// @dev only the owner of the token can add an address.
+    /// @param addressToRemove_ The address to remove from sanction list.
+    function removeAddressFromSanctionsList(address addressToRemove_) external onlyOwner {
         _sanctionedAddresses[addressToRemove_] = false;
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+    /// @notice Checks if an address is sanctioned.
+    /// @param addressToCheck_ The address for which to check sanction status.
+    function isSanctioned(address addressToCheck_) public view returns (bool) {
+        return _sanctionedAddresses[addressToCheck_];
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
         bool isFromSanctioned = _sanctionedAddresses[from];
         bool isToSanctioned = _sanctionedAddresses[to];
-        require(isFromSanctioned || isToSanctioned, "address used in transfer is sanctioned");
+        require(!(isFromSanctioned || isToSanctioned), "address used in transfer is sanctioned");
         super._beforeTokenTransfer(from, to, amount);
     }
 }
--- test/SanctionableToken.t.sol
+++ test/SanctionableToken.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {SanctionableToken} from "../src/SanctionableToken.sol";
 
 contract SanctionableTokenTest is Test {
@@ -15,25 +15,72 @@ contract SanctionableTokenTest is Test {
         _bob = address(2);
         vm.label(_bob, "Bob");
         token = new SanctionableToken("USD", "USD");
+        token.mint(_alice, 10_000);
+        token.mint(_bob, 5_000);
     }
 
-    function testTokenName() public {
-        assertEq(token.name(), "USD");
+    function testCanTransferFunds() public {
+        vm.prank(_alice);
+        token.transfer(_bob, 100);
+        assertEq(token.balanceOf(_bob), 5_100);
     }
 
-    function testCanTransferFunds() public {}
+    function testAdminCanSanctionAddress() public {
+        token.addAddressToSanctionsList(_bob);
+        assertEq(token.isSanctioned(_bob), true);
+    }
 
-    function testAdminCanSanctionAddress() public {}
+    function testNonAdminCannotSanctionAddress() public {
+        vm.prank(_alice);
+        vm.expectRevert();
+        token.addAddressToSanctionsList(_bob);
+    }
 
-    function testNonAdminCannotSanctionAddress() public {}
+    function testSanctionedAddressCantTransferFunds() public {
+        token.addAddressToSanctionsList(_bob);
+        assertEq(token.isSanctioned(_bob), true);
+        vm.prank(_bob);
+        vm.expectRevert();
+        token.transfer(_alice, 100);
+    }
 
-    function testSanctionedAddressCantTransferFunds() public {}
+    function testSanctionedAddressCantReceiveFunds() public {
+        token.addAddressToSanctionsList(_bob);
+        assertEq(token.isSanctioned(_bob), true);
+        vm.prank(_alice);
+        vm.expectRevert();
+        token.transfer(_bob, 100);
+    }
 
-    function testSanctionedAddressCantReceiveFunds() public {}
+    // can't sanction zero address because it's used in minting and burning
+    function testCantSanctionZeroAddress() public {
+        vm.expectRevert();
+        token.addAddressToSanctionsList(address(0));
+    }
 
-    function testCantSanctionZeroAddress() public {}
+    function testSanctionedAddressCantTransferFrom() public {
+        token.addAddressToSanctionsList(_bob);
+        assertEq(token.isSanctioned(_bob), true);
+        vm.prank(_alice);
+        token.approve(address(3), 100);
+        assertEq(token.allowance(_alice, address(3)), 100);
+        vm.prank(address(3));
+        vm.expectRevert();
+        token.transferFrom(_alice, _bob, 100);
+    }
 
-    function testSanctionedAddressCantTransferFrom() public {}
-
-    function testRemoveSanctionedAddressCanTransfer() public {}
+    function testRemoveSanctionedAddressCanTransfer() public {
+        token.addAddressToSanctionsList(_bob);
+        assertEq(token.isSanctioned(_bob), true);
+        vm.prank(_alice);
+        token.approve(address(3), 100);
+        assertEq(token.allowance(_alice, address(3)), 100);
+        vm.prank(address(3));
+        vm.expectRevert();
+        token.transferFrom(_alice, _bob, 100);
+        token.removeAddressFromSanctionsList(_bob);
+        vm.prank(address(3));
+        token.transferFrom(_alice, _bob, 100);
+        assertEq(token.balanceOf(_bob), 5_100);
+    }
 }
